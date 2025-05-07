f9a246bfe7d9a8bcff2f347031409dd47ec6da57
shravani8005/ <shravani8005@gmail.com>
2025-05-07 03:08:59 -0700

    save

---
+++ operations.java
@@ -0,0 +1,10 @@
+public class operations {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+    }
+}
