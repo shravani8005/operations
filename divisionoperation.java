7d6604f1aa1f9f8dd25663e9626e5d675fbbe362
shravani8005/ <shravani8005@gmail.com>
2025-05-07 03:14:08 -0700

    save

---
+++ divisionoperation.java
@@ -0,0 +1,13 @@
+public class divisionoperation {
+    public static void main(String[] args){
+    int a =15;
+    int b = 0;
+    try{
+        int div = a /b;
+        System.out.println("Division:"+div);
+    }
+    catch (ArithmeticException e){
+        System.out.println("Error:connot divide by zero");
+    }
+    }
+}
